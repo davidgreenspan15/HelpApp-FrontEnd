561a7c15b40ce9bdc8dcf424bea55c8f417ffcc4
Christian Pinto <pintochristian2@gmail.com>
2019-07-25 15:50:43 -0400

    navbar

---
+++ help-app-frontend/src/containers/NavBar.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import SearchBar from '../components/SearchBar'
+
+class Navbar extends React.Component{
+  state = {
+    search: "",
+    loggedIn: false
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      search: event.target.value
+    }, () => console.log(this.state.search))
+  }
+
+  handleSearch = () => {
+    console.log("Here")
+  }
+
+  render(){
+    return(
+      <div className="nav-container">
+        <i class="dollar sign icon"></i>
+        <div className="company-name">
+          Help
+        </div>
+        <SearchBar/>
+        {
+          this.state.loggedIn ?
+          null
+          :
+          <span>
+            <button className="ui red button login">Login</button>
+            <button className="ui green button">Signup</button>
+          </span>
+
+        }
+        {
+          this.state.loggedIn
+          ? <button className="ui yellow button"><i class="user outline icon"></i></button>
+            : null
+        }
+        <button className="ui blue button new-event">
+          <i class="calendar plus icon"></i>
+        </button>
+      </div>
+        )
+      }
+    }
+
+export default Navbar;
