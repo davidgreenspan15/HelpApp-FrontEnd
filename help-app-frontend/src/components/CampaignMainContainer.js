13120f0af1fac61cae83516b8e6f0ebb3adf18e2
Christian Pinto <pintochristian2@gmail.com>
2019-07-31 09:28:43 -0400

    changes

--- help-app-frontend/src/components/CampaignMainContainer.js
+++ help-app-frontend/src/components/CampaignMainContainer.js
@@ -5,7 +5,7 @@ class CampaignMainContainer extends React.Component{
   render(){
     return(
       <div className="left-side">
-        <img className="campaign-image" src="https://cdn.pixabay.com/photo/2016/12/13/05/15/puppy-1903313__340.jpg" alt=""/>
+        <img className="campaign-image" src={this.props.campaign.image} alt=""/>
         <h3>Created On: {this.props.campaign.created_at.split('T')[0]}</h3>
         <div className="description">{this.props.campaign.description}</div>
       </div>
